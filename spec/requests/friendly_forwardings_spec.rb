2ea74eb464b8f61ff9c433eb6a3baeee1fbd9a5a
Hani Elabed <hani.elabed@gmail.com>
2011-04-13 13:37:52 -0500

    now non-logged in user are redirected to correct requested page after logging in

---
+++ spec/requests/friendly_forwardings_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "FriendlyForwardings" do
+
+
+  it "should forward to the requested page after signin" do
+    user = Factory(:user)
+    visit edit_user_path(user)
+    # The test automatically follows the redirect to the signin page.
+    # the integration test follows redirects, so testing that the
+    # response should redirect_to some URL won’t work.
+    # I learned this the hard way
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+    # The test follows the redirect again, this time to users/edit.
+    response.should render_template('users/edit')
+  end
+
+end
